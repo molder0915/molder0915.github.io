1db9556c3baee5e4eed2623c96b7c1af6c029382
molder <2414666298@qq.com>
2023-04-10 19:57:26 +0800

    Site updated: 2023-04-10 19:57:25

--- js/foot.js
+++ js/foot.js
@@ -1,6 +1,6 @@
 // 动态心跳，更改自己的名称
 $(document).ready(function(e){
-    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 阿散');
+    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 木勒');
 })
 
 $(document).ready(function(e){
@@ -11,7 +11,7 @@ $(document).ready(function(e){
 function show_date_time(){
 $('.framework-info').html('小破站已经安全运行<span id="span_dt_dt" style="color: #fff;"></span>');
 window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("2/1/2023 0:0:0");
+BirthDay=new Date("2/7/2023 10:17:09");
 today=new Date();
 timeold=(today.getTime()-BirthDay.getTime());
 sectimeold=timeold/1000
